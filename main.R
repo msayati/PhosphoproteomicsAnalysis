ae2a62e2065456e5fbdb8a3502fa43fccb3d92d0
daniel235 <daduncan298@gmail.com>
2019-03-18 18:16:48 -0500

    removed unncessary code from main file

--- main.R
+++ main.R
@@ -15,25 +15,4 @@ dim(breastCancerData)
 
 bdata <- matrix(c(breastCancerData), nrow=56874, ncol=29, byrow=TRUE)
 
-dim(bdata)
 
-print(bdata[[1]][1])
-
-for(x in bdata[[3]]){
-  if(x == "NA"){
-    x = 0
-    print(x)
-  }
-}
-
-for(r in 1:nrow(bdata)){
-  for(c in 1:ncol(bdata)){
-    if(bdata[r,c] == "NA") {
-      bdata[r,c] = 0 
-    }
-  }
-}
-
-bdata[[1:29]] 
-
-print(bdata[[3]])
