a644bb0020a247cc8aee26fd9224a591b6557764
daniel235 <daduncan298@gmail.com>
2019-03-18 18:45:51 -0500

    added kinase correlation file(starting import data)

---
+++ kinase_correlation.R
@@ -0,0 +1,13 @@
+#Read the KSA_human file in the data
+kinase_human <- read.table("data/KSA_human.txt", sep="", fill=TRUE)
+
+#clean data 
+kinase_human <- kinase_human[!apply(kinase_human, 1, function(x) any(x == "")),]
+
+#Create a list of unique kinase
+
+
+#For each kinase, find its substrates in the phosphorylation data 
+#and compute the correlation between all of them
+
+
