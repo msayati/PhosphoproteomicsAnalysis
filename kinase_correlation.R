c524a396de4f15032c250943b59f48501e3a1ba7
daniel <daduncan298@gmail.com>
2019-03-18 19:52:04 -0500

    added unique list

--- kinase_correlation.R
+++ kinase_correlation.R
@@ -5,9 +5,13 @@ kinase_human <- read.table("data/KSA_human.txt", sep="", fill=TRUE)
 kinase_human <- kinase_human[!apply(kinase_human, 1, function(x) any(x == "")),]
 
 #Create a list of unique kinase
-
+kinase_names = unique(kinase_human[1])
 
 #For each kinase, find its substrates in the phosphorylation data 
 #and compute the correlation between all of them
+
+
+
+
 
 
