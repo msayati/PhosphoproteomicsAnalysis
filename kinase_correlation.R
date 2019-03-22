fe88d2d7c3ecebb27c6654dd49b55c2a18d49aee
Hasbanny Irisson <hasbanny@aol.com>
2019-03-22 02:53:49 -0500

    changing variable names + concatenating columns in files

--- kinase_correlation.R
+++ kinase_correlation.R
@@ -5,16 +5,17 @@ library(magrittr)
 library(tidyr)
 
 #Read the KSA_human file in the data
-kinase_human <- read.table("data/KSA_human.txt", sep="", fill=TRUE)
+kinase_human <- read.table("data/KSA_human.txt", header = TRUE, sep="", fill=TRUE)
 
 #clean data 
 kinase_human <- kinase_human[!apply(kinase_human, 1, function(x) any(x == "")),]
 
 #Create a list of unique kinase
-kinase_names = unique(kinase_human$V1)
+kinase_names = unique(kinase_human$Kinase)
 
 #concatenating substrate and site in kinase_human for easy retrieval
-kinase_human %<>% unite(V2, V2, V3, remove = TRUE)
+kinase_human %<>% unite(SubstrateSite, Substrate, Site, sep="-", remove = TRUE)
+cleanBCD %<>% unite(geneSymbol_Site, geneSymbol, variableSites, sep="-", remove = TRUE)
 
 #For each kinase, find its substrates in the phosphorylation data 
 #and compute the correlation between all of them
