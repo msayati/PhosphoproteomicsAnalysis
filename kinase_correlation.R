ce87548804159fd646fae0fd4082a8a11f90ce81
Hasbanny Irisson <hasbanny@aol.com>
2019-03-22 13:58:17 -0500

    finding substrate-site for each unique kinase

--- kinase_correlation.R
+++ kinase_correlation.R
@@ -11,16 +11,19 @@ kinase_human <- read.table("data/KSA_human.txt", header = TRUE, sep="", fill=TRU
 kinase_human <- kinase_human[!apply(kinase_human, 1, function(x) any(x == "")),]
 
 #Create a list of unique kinase
-kinase_names = unique(kinase_human$Kinase)
+kinase_names <- unique(kinase_human[1])
 
 #concatenating substrate and site in kinase_human for easy retrieval
 kinase_human %<>% unite(SubstrateSite, Substrate, Site, sep="-", remove = TRUE)
 cleanBCD %<>% unite(geneSymbol_Site, geneSymbol, variableSites, sep="-", remove = TRUE)
 
-#For each kinase, find its substrates in the phosphorylation data 
-#and compute the correlation between all of them
-
-
+#For each unique kinase(k is the actual kinase, not an index)
+for (k in kinase_names$Kinase){
+  #find the substrate and site from kinase_human and store in subs
+  print(paste0("kinase: ", k))
+  subs <- kinase_human$SubstrateSite[which(k == kinase_human$Kinase)]
+  print(subs)
+}
 
 
 
