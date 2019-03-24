ef1f6eff15c280cd4c5b45dc78f5e0d8648f7bd6
Cynthia Aguillon <caguillon.techie@gmail.com>
2019-03-24 18:13:53 -0500

    Cleaning data is now a function

--- cleaning.R
+++ cleaning.R
@@ -4,18 +4,27 @@ install.packages("readxl")
 #setting library used
 library(readxl)
 
-#reads in excel file
-BreastCancerData1 <- read_excel("data/BreastCancerData.xlsx",sheet = "S5.iTRAQ_phosphoproteome")
+# function to clean breast cancer data given the file location as a string
+clean.bcd<-function(bcd){
+  
+  #reads in excel file
+  BreastCancerData1 <- read_excel(bcd,sheet = "S5.iTRAQ_phosphoproteome")
+  
+  #converts it into csv file
+  write.csv(BreastCancerData1,"data/BreastCancerData.csv",row.names=FALSE)
+  
+  #reads in csv file
+  BreastCancerData1 <- read.csv("data/BreastCancerData.csv")
+  
+  #creating a dataframe cleanBCD to store the columns that have more
+  #less than 40%NA
+  cleanBCD <- BreastCancerData1[ lapply( BreastCancerData1, function(cleanBCD) sum(is.na(cleanBCD)) / length(cleanBCD) ) < .40 ]
+  
+  #delete rows with NA
+  cleanBCD <- cleanBCD[complete.cases(cleanBCD), ]
+  
+  return(cleanBCD)
+}
 
-#converts it into csv file
-write.csv(BreastCancerData1,"data/BreastCancerData.csv",row.names=FALSE)
-
-#reads in csv file
-BreastCancerData1 <- read.csv("data/BreastCancerData.csv")
-
-#creating a dataframe cleanBCD to store the columns that have more
-#less than 40%NA
-cleanBCD <- BreastCancerData1[ lapply( BreastCancerData1, function(cleanBCD) sum(is.na(cleanBCD)) / length(cleanBCD) ) < .40 ]
-
-#delete rows with NA
-cleanBCD <- cleanBCD[complete.cases(cleanBCD), ]
+file <- "data/BreastCancerData.xlsx"
+cleanBCD <- clean.bcd(file)
