74836e39ed0dc129302eaed897531e477a46929f
Hasbanny Irisson <hasbanny@aol.com>
2019-03-26 22:54:28 -0500

    removing last char from cleanBCD + finding intersection of shared kinases in breast cancer data

--- kinase_correlation.R
+++ kinase_correlation.R
@@ -17,12 +17,22 @@ kinase_names <- unique(kinase_human[1])
 kinase_human %<>% unite(SubstrateSite, Substrate, Site, sep="-", remove = TRUE)
 cleanBCD %<>% unite(geneSymbol_Site, geneSymbol, variableSites, sep="-", remove = TRUE)
 
+#removing the last char from cleanBCD
+cleanBCD$geneSymbol_Site = substr(cleanBCD$geneSymbol_Site,1,nchar(cleanBCD$geneSymbol_Site)-1)
+
 #For each unique kinase(k is the actual kinase, not an index)
 for (k in kinase_names$Kinase){
   #find the substrate and site from kinase_human and store in subs
   print(paste0("kinase: ", k))
   subs <- kinase_human$SubstrateSite[which(k == kinase_human$Kinase)]
-  print(subs)
+  #subs <- as.factor(subs)
+  #print(subs)
+  
+  subsinexp <- intersect(subs, cleanBCD$geneSymbol_Site)
+  if(length(subsinexp) == 0){
+    subsinexp <- 0
+  }
+  print(paste0("subsinexp: ", subsinexp))
 }
 
 
