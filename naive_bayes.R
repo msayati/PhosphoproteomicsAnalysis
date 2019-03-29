d2b44f16429b98ed5387c830ead57a79041f3c27
daniel235 <daduncan298@gmail.com>
2019-03-29 01:45:24 -0500

    adding graph class

--- naive_bayes.R
+++ naive_bayes.R
@@ -4,22 +4,31 @@ library(tidyr)
 
 #assuming data is vector of vectors
 build_graph <- function(){
-  #grab all sites
-  #combine gene and site name
-  P <- paste(cleanBCD$geneSymbol, cleanBCD$variableSites, sep="-")
-  #remove last letter off of site
-  P <- substr(P, 1, nchar(P)-1)
-  #get unique sites(not repeated)
-  P <- unique(P)
-  print(length(P))
+  newGraph <- list(
+    edges <- list(),
+    nodes <- list()
+  )
+  newGraph$grab_sites = function(){
+    #grab all sites
+    #combine gene and site name
+    P <- paste(cleanBCD$geneSymbol, cleanBCD$variableSites, sep="-")
+    #remove last letter off of site
+    P <- substr(P, 1, nchar(P)-1)
+    #get unique sites(not repeated)
+    P <- unique(P)
+    print(length(P))
+    newGraph$nodes <- as.vector(P)
+  }
   
-  #create nodes
+  #create nodes(turns out R is object oriented)
+  return(newGraph)
   
 }
 
 #get x and y vectors 
 naive_bayes <- function(){
-  
+  graph <- build_graph()
 } 
 
-build_graph()
+naive_bayes()
+
