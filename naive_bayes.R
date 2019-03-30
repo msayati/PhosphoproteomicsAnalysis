f4b8b99cf4d06d3c4399d832e95813dce348ae8b
daniel235 <daduncan298@gmail.com>
2019-03-30 01:21:14 -0500

    removed graph

--- naive_bayes.R
+++ naive_bayes.R
@@ -2,33 +2,17 @@ library("WGCNA")
 library(tidyr)
 
 
-#assuming data is vector of vectors
-build_graph <- function(){
-  newGraph <- list(
-    edges <- list(),
-    nodes <- list()
-  )
-  newGraph$grab_sites = function(){
-    #grab all sites
-    #combine gene and site name
-    P <- paste(cleanBCD$geneSymbol, cleanBCD$variableSites, sep="-")
-    #remove last letter off of site
-    P <- substr(P, 1, nchar(P)-1)
-    #get unique sites(not repeated)
-    P <- unique(P)
-    print(length(P))
-    newGraph$nodes <- as.vector(P)
+naive_bayes <- function(S, A){
+  #S -> [.8, .9, .3]
+  #A -> [.3, .4, .5, .4, .2, .... ]
+  sharedLength <- length(S)
+  
+  for(psite in unknownSites){
+    for(k in S){
+      
+    }
   }
   
-  #create nodes(turns out R is object oriented)
-  return(newGraph)
   
-}
-
-#get x and y vectors 
-naive_bayes <- function(){
-  graph <- build_graph()
 } 
-
-naive_bayes()
 
