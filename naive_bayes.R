f9f0ca93d98558c77c18d02b9bdb0b0ed989ff9b
daniel235 <daduncan298@gmail.com>
2019-03-31 18:43:42 -0500

    gettin psites (working on bug)

--- naive_bayes.R
+++ naive_bayes.R
@@ -21,12 +21,10 @@ grab_substrates <- function(k){
     cleanBCD$geneSymbol_Site[counter] <- str_sub(sym, end=-2)
     counter <- counter + 1
   }
+
   
-  
-  print(k)
   #find the substrate and site from kinase_human and store in subs
   substrates <- unique(kinase_human$SubstrateSite[which(k == kinase_human$Kinase)])
-  print(substrates)
   #find the intersection of substrate-site in breast cancer data:
   #1. find the indices where the substrates-site from that kinase, match in BCD
   
@@ -36,25 +34,42 @@ grab_substrates <- function(k){
   subsinexp1<-subsinexp1[!is.na(subsinexp1)]
   #print(subsinexp1)
   pSites <- cleanBCD[subsinexp1,]
+  
+  return(pSites)
 }
 
-
-
-naive_bayes <- function(){
+naive_bayes <- function(){ 
   #S -> [.8, .9, .3]
   #A -> [.3, .4, .5, .4, .2, .... ]
-  sharedLength <- length(S)
-  
-  for(psite in unknownSites){
-    for(k in kinase_names){ 
+  #sharedLength <- length(S)
+  count <- 0
+  for(psite in 1:length(cleanBCD)){
+    p <- cleanBCD[psite,]
+    
+    for(k in kinase_names$Kinase){ 
+      print(k)
       #grab substrates for k (while we figure out memory problem)
-      grab_substrates(k)
+      kinase_sites <- grab_substrates(k)
+      count <- count + 1
+      if(nrow(kinase_sites) != 0) {
+        print(kinase_sites)
+        c <- c()
+        for(ks in kinase_sites){
+          #compute bicor correlation between sites and psite
+          append(c, bicor(p, ks))
+          
+          #naive bayes algorithm
+          
+        }
+      }
       
     }
   }
-  grab_substrates(kinase_names$Kinase[[57]])
+  
   
 } 
+
+grab_substrates(kinase_names$Kinase[[124]])
 
 naive_bayes()
 
