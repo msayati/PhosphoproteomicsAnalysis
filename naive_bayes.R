32dae6a8f84588b231d4310b5e3d441b290f940a
daniel235 <daduncan298@gmail.com>
2019-04-02 03:43:17 -0500

    fixed bicor bug

--- naive_bayes.R
+++ naive_bayes.R
@@ -9,39 +9,22 @@ library(dplyr)
 library(stringr)
 
 grab_substrates <- function(k){
-  #remove last char on bcdata
-  #geneSymbolSite <- paste(cleanBCD$geneSymbol, cleanBCD$variableSites, sep="-")
-  #cleanBCD$geneSymbol <- geneSymbolSite
-  #cleanBCD <- subset(cleanBCD, select=-variableSites)
-  
-  #rename column
-  #names(cleanBCD) <- c("geneSymbol_Site")
-  #counter = 1
-  #for (sym in cleanBCD$geneSymbol_Site){
-    #cleanBCD$geneSymbol_Site[counter] <- str_sub(sym, end=-2)
-    #counter <- counter + 1
-  #}
-
-  
   #find the substrate and site from kinase_human and store in subs
   substrates <- kinase_human$SubstrateSite[which(k == kinase_human$Kinase)]
-  #print(substrates)
   #find the intersection of substrate-site in breast cancer data:
   #1. find the indices where the substrates-site from that kinase, match in BCD
   
   subsinexp1 <- match(substrates, cleanBCD$geneSymbol_Site)
-  #print(subsinexp1)
+  
   #2. if the substrate-site isnt found, remove na
   subsinexp1<-subsinexp1[!is.na(subsinexp1)]
-  #print(subsinexp1)
+  
   pSites <- cleanBCD[subsinexp1,]
   pSites <- as.vector(pSites)
   return(pSites)
 }
 
 naive_bayes <- function(){ 
-  #S -> [.8, .9, .3]
-  #A -> [.3, .4, .5, .4, .2, .... ]
   #sharedLength <- length(S)
   c <- vector()
   for(psite in 1:length(cleanBCD)){
@@ -55,13 +38,13 @@ naive_bayes <- function(){
       
       if(nrow(kinase_sites) != 0) {
         for(i in nrow(kinase_sites)){
-          #print(kinase_sites[i,], max.levels=0)
           #compute bicor correlation between sites and psite
           
-          kinase_sites[i,] <- as.vector(unlist(kinase_sites[i,2:ncol(kinase_sites[i,])]))
-    
-          #print(kinase_sites[i,])
-          print(bicor(p, kinase_sites[i,]))
+          ksite <- as.vector(unlist(kinase_sites[i,]))
+          ksite <- ksite[2:length(ksite)]
+          
+          print(bicor(p, ksite))
+          append(c, bicor(p, ksite))
           #naive bayes algorithm
           
         }
