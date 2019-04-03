477e8a8cbb35abf501f86870353e20fece76b5a8
daniel <daduncan298@gmail.com>
2019-04-03 17:33:52 -0500

    rank update

--- naive_bayes.R
+++ naive_bayes.R
@@ -27,31 +27,20 @@ grab_substrates <- function(k){
 
 naive_bayes <- function(S, A){ 
   #Read the KSA_human file in the data
-  kinase_human <- read.table("data/KSA_human.txt", header = TRUE, sep="", fill=TRUE)
   
-  #clean data 
-  kinase_human <- kinase_human[!apply(kinase_human, 1, function(x) any(x == "")),]
-  
-  #Create a list of unique kinase
-  kinase_names <- unique(kinase_human[1])
-  
-  #concatenating substrate and site in kinase_human for easy retrieval
-  kinase_human %<>% unite(SubstrateSite, Substrate, Site, sep="-", remove = TRUE)
-  cleanBCD %<>% unite(geneSymbol_Site, geneSymbol, variableSites, sep="-", remove = TRUE)
-  
-  #removing the last char from cleanBCD
-  cleanBCD$geneSymbol_Site = substr(cleanBCD$geneSymbol_Site,1,nchar(cleanBCD$geneSymbol_Site)-1)
   #sharedLength <- length(S)
   #summation term
   Sum <- 0
-  kinaseRank <- c()
+  index <- 0
+  kinaseRank <- matrix(ncol=2)
   for(psite in 1:length(cleanBCD)){
     p <- cleanBCD[psite,]
     #convert to vector and remove kinase column
     p <- as.vector(unlist(p))
     p <- p[2:length(p)]
-    
+    index <- 0
     for(k in kinase_names$Kinase){ 
+      index <- index + 1
       Sum <- 0
       #grab substrates for k (while we figure out memory problem)
       kinase_sites <- grab_substrates(k)
@@ -61,22 +50,17 @@ naive_bayes <- function(S, A){
           ksite <- as.vector(unlist(kinase_sites[i,]))
           ksite <- ksite[2:length(ksite)]
           c <- bicor(p, ksite)
-          
           ########### naive bayes algorithm ###############
           #compare c with every S and A
           Sprob <- length(which(as.vector(unlist(lapply(S, function(x) c > x))))) + 1
           
           Aprob <- length(which(as.vector(unlist(lapply(A, function(x) c > x))))) + 1
           
-          print(Sprob)
-          print(Aprob)
-          
           Sum <- Sum + log2(Sprob/Aprob)
-          print(Sum)
           
         }
         #add sum for kinase i in kinaseRank vector
-        kinaseRank <- c(kinaseRank, Sum)
+        kinaseRank <- rbind(kinaseRank, c(index, Sum))
         
       }
     }
@@ -93,5 +77,6 @@ naive_bayes(S, A)
 
 
 
+m <- matrix(data=c(1,3,3,4,5,6,3,8,3), nrow=3, ncol=3)
 
 
