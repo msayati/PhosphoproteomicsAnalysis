ec1581a265413f5cf13b85f24240d6eb4ee589ab
Cynthia Aguillon <caguillon.techie@gmail.com>
2019-04-03 11:25:02 -0500

    Working on uploading data to app

--- app/app.R
+++ app/app.R
@@ -1,18 +1,32 @@
 # Required libraries
-
 library(shiny)
+
+# Changes shiny limit file upload to 40MB
+options(shiny.maxRequestSize = 40*1024^2)
 
 # UI for app
 ui <- fluidPage(
    
-   # App title
-   titlePanel("Phosphoproteomics Analysis")
-   
+  # App title
+  titlePanel("Phosphoproteomics Analysis"),
+  
+  # Website layout
+  sidebarLayout(
+    # our inputs will go here
+    sidebarPanel(
+      fileInput("file", "Choose Excel File", multiple = FALSE,
+                accept = c(".xlsx"),
+                width = NULL, buttonLabel = "Browse...",
+                placeholder = "No file selected")
+    ),
+    # results will go here
+    mainPanel(tableOutput("contents"))
+  )
 )
 
-# Server code
+# Server code for app
 server <- function(input, output) {
-   
+  #FIX: inFile <- input$file
 }
 
 # Run the application 
