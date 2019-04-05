6ea4f55ad6cd3a38e88723c9ecc1637ed881a57f
daniel <daduncan298@gmail.com>
2019-04-04 22:51:33 -0500

    adding data frame

--- naive_bayes.R
+++ naive_bayes.R
@@ -25,7 +25,13 @@ grab_substrates <- function(k){
   return(pSites)
 }
 
+#add to a data frame
+build_table <- function(table, site, newKRank){
+  
+}
 
+#returns the final table of all kinase correlations with psite
+#and returns the top N rank in a data frame with kinases as columns
 final_table <- function(krankMatrix, topCount){
   #create table of all kinase ranks
   
@@ -34,16 +40,24 @@ final_table <- function(krankMatrix, topCount){
 }
 
 naive_bayes <- function(S, A, topCount){ 
-  #Read the KSA_human file in the data
-  
+  ########## required data structures ###########
   sharedLength <- length(S)
   allLength <- length(A)
   #summation term
   Sum <- 0
   index <- 0
   kinaseRank <- matrix(ncol=2)
+  currentTable <- data.frame()
+  
+  
+  ###############################################
+  
+  
   for(psite in 1:nrow(cleanBCD)){
     p <- cleanBCD[psite,]
+    #site name 
+    pSiteName <- unlist(p[1])
+    print(pSiteName)
     #convert to vector and remove kinase column
     p <- as.vector(unlist(p))
     p <- p[2:length(p)]
@@ -54,6 +68,7 @@ naive_bayes <- function(S, A, topCount){
       #grab substrates for k (while we figure out memory problem)
       kinase_sites <- grab_substrates(k)
       if(nrow(kinase_sites) != 0) {
+        #set current table column names
         for(i in nrow(kinase_sites)){
           #compute bicor correlation between sites and psite
           ksite <- as.vector(unlist(kinase_sites[i,]))
@@ -67,11 +82,11 @@ naive_bayes <- function(S, A, topCount){
           
           Sum <- Sum + log2((Sprob/sharedLength)/(Aprob/allLength))
           
-          
         }
         #add sum for kinase i in kinaseRank matrix with index of kinase(kinase_names)
         kinaseRank <- rbind(kinaseRank, c(index, Sum))
-        
+        currentTable[pSiteName, k] <- Sum
+       # currentTable <- build_table(currentTable, pSiteName, kinaseRank)
         
       }
     }
@@ -79,15 +94,14 @@ naive_bayes <- function(S, A, topCount){
   }
   #print(kinaseRank)
   print(length(kinaseRank))
+  print(currentTable)
+  return (currentTable)
   
 } 
 
 #placeholder vectors
 S <- c(.5, .4, .3, .8, .7)
 A <- c(.2, .3, .8, .3, .3, .2, .1, .1, .1, .7, .5, .4, .3, .4, .3, .1, .01, .01, .01, .01, .01)
-naive_bayes(S, A, 3)
-
-
-
+table <- naive_bayes(S, A, 3)
 
 
