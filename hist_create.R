891c99012faa8d863b61e4425704048b7b785374
Hasbanny Irisson <hasbanny@aol.com>
2019-04-07 11:47:49 -0500

    function for 'Vector A' made

--- hist_create.R
+++ hist_create.R
@@ -7,15 +7,21 @@ biocLite(c("GO.db", "preprocessCore", "impute"))
 
 library(WGCNA)
 
-#drops the first two columns because theyre not numeric value
-cleanBCD <- cleanBCD[,-c(1,2)]
+all_paircorr <- function(cbcd){
+  #drops the first two columns because theyre not numeric value
+  cbcd <- cbcd[,-c(1,2)]
 
-#does the correlation and saves it in dataframe corr
-corr <- bicor(t(cleanBCD))
+  #does the correlation and saves it in dataframe corr
+  corr <- bicor(t(cbcd))
 
-#dataframe upp contains the upper triangle of corr
-upp <- upper.tri(corr, diag = FALSE)
-all_corr <- corr[upp]
+  #dataframe upp contains the upper triangle of corr
+  upp <- upper.tri(corr, diag = FALSE)
+  all_corr <- corr[upp]
+  
+  return(all_corr)
+}
+
+all_corr <- all_paircorr(cleanBCD)
 
 #creates histogram of data
 hist(all_corr)
