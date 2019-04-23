9bc2d3d093b51699704b859fb79a0dee47284107
Hasbanny Irisson <hasbanny@aol.com>
2019-04-23 15:54:53 -0500

    top 10 negative/positive Kinases

--- KSEA.R
+++ KSEA.R
@@ -11,21 +11,37 @@ kinase_score = vector('numeric')
 for (k in kinase_names$Kinase){
   
   kinase_sites <-grab_substrates(k, cleanBCD, kinase_human)
-  
+
   #find subsinexp in logFC
-  if(nrow(kinase_sites) != 0){
-    
+   if(nrow(kinase_sites) > 0){
+
     #getting indices where subsinexp is found in logFC
     indx <- match(kinase_sites$geneSymbol_Site, logFC$geneSymbol_Site)
-    
+
     #storing the log2FC of subsinexp found in logFC
     commonFC <-logFC$log2FC[indx]
     
-    #compute score of Kinase
+    #compute score of Kinase and store it in vector
     kinase_score = append(kinase_score,((mean(commonFC) - mean(logFC$log2FC))*sqrt(length(subsinexp12)))/sd(logFC$log2FC), after = length(kinase_score))
+    
+   }
+  else {
+    #if kinase has no substrates in experimental data, the score is 0
+    kinase_score = append(kinase_score, 0, after = length(kinase_score))
   }
 }
 
-#sorting in ascending order
-sorted_kinaseScores = sort(kinase_score, decreasing = FALSE)
+#creating dataframe with kinase name and their respective score
+kscore1 <- data.frame(kinase_names[1], score = kinase_score)
 
+#splitting kscore1 into negative and positive score
+neg <- kscore1[which(kscore1$score < 0),]
+pos <- kscore1[which(kscore1$score > 0),]
+
+#ordering neg in descending order and pos in ascending
+neg <- neg[with(neg, order(score, decreasing = TRUE)),]
+pos <- pos[with(pos, order(score, decreasing = FALSE)),]
+
+#getting the top 10 of neg and top 10 of pos
+top10neg <- top10neg[1:10,]
+top10pos <- top10pos[1:10,]
