da0b1f6a6c87030ba6365fb9e660dbad3854a9e1
daniel <daduncan298@gmail.com>
2019-04-24 01:42:00 -0500

    finished bicor for random data

--- random.R
+++ random.R
@@ -1,15 +1,39 @@
+
+
 #row randomization
 randomPlot <- function(dataArray){
+  #remove first column
+  dataArray <- dataArray[-c(1)]
   #generate random data
-  arraySize = nrow(dataArray)
-  randArray = sample(1:arraySize, 2, replace=F)
+  arraySize <- nrow(dataArray)
+  colSize <- ncol(dataArray)
+  cNames <- colnames(dataArray)
+  newDataFrame <- data.frame()
+  randArray <- sample(1:arraySize, 3, replace=F)
+  randArray = lapply(randArray, function(x) dataArray[x,])
   
-  randArray = lapply(randArray, function(x) dataArray[x])
+  print(length(randArray))
+  #get random matrix to compute bicors
+  randomMatrix <- matrix(ncol=colSize)
+  
+  for(i in 1:length(randArray)){
+    r <- as.vector(unlist(randArray[i]))
+    print(r)
+    randomMatrix <- rbind(randomMatrix, c(r)) 
+  }
+  #remove first element
+  randomMatrix <- randomMatrix[-1,]
+  
+  print(randomMatrix)
+  
   #get bicors correlation values
-  print(randArray)
+  bcors <- bicor(randomMatrix)
+  bcorsIndex <- upper.tri(bcors, diag = FALSE)
+  bcors <- bcors[bcorsIndex == TRUE]
+  print(bcors)
   
   #normalize y axis
-  
+  hist(bcors)
   #plot correlation values
   
   #plot all pairs/shared-kinase pairs/randomized al pairs
@@ -17,11 +41,12 @@ randomPlot <- function(dataArray){
   #plot historgram
 }
 
-realOne = sample(1:20, 4, replace=F)
-realTwo = sample(1:20, 4, replace=F)
-realThree = sample(1:20, 4, replace=F)
+realOne = sample(1:20, 8, replace=F) 
+realTwo = sample(1:20, 8, replace=F)
+realThree = sample(1:20, 8, replace=F)
+realFour = sample(1:20, 8, replace=F)
 
-data = data.frame(one = realOne, two = realTwo, three = realThree)
+data = data.frame(one = realOne, two = realTwo, three = realThree, four = realFour)
 
 print(data)
 randomPlot(data)
