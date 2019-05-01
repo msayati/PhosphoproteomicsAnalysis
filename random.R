ddb03a95a799f60e031b6583135a115e2ce53c21
daniel <daduncan298@gmail.com>
2019-05-01 15:43:06 -0500

    fixed labels

--- random.R
+++ random.R
@@ -1,67 +1,59 @@
 library(ggplot2)
+
+
 #row randomization
+#function takes in data frame
 randomPlot <- function(dataArray, allCorr, sharedKinaseCorr){
-  #remove first column
+  print(dataArray)
+  #remove first column(psite name)
   dataArray <- dataArray[-c(1)]
-  #generate random data
+  
   arraySize <- nrow(dataArray)
   colSize <- ncol(dataArray)
   cNames <- colnames(dataArray)
   newDataFrame <- data.frame()
-  randArray <- sample(1:arraySize, 3, replace=F)
+  
+  #generate random data
+  randArray <- sample(1:arraySize, 100, replace=FALSE)
   randArray = lapply(randArray, function(x) dataArray[x,])
   
-  print(length(randArray))
   #get random matrix to compute bicors
   randomMatrix <- matrix(ncol=colSize)
   
+  #remove column names from individual rows 
   for(i in 1:length(randArray)){
     r <- as.vector(unlist(randArray[i]))
-    print(r)
+    #add individual rows to random matrix
     randomMatrix <- rbind(randomMatrix, c(r)) 
   }
-  #remove first element
+  #remove first row
   randomMatrix <- randomMatrix[-1,]
   
-  print(randomMatrix)
   
-  #get bicors correlation values
+  #get random matrix bicors correlation values
   bcors <- bicor(randomMatrix)
   bcorsIndex <- upper.tri(bcors, diag = FALSE)
   bcors <- bcors[bcorsIndex == TRUE]
-  print(bcors)
   
   #convert to data frame
   bcors <- data.frame(bcors)
   colnames(bcors) <- c("x")
-  #names(bcors) <- NULL
   allCorr <- data.frame(allCorr)
   colnames(allCorr) <- c("x")
-  #names(allCorr) <- NULL
   sharedKinaseCorr <- data.frame(sharedKinaseCorr)
   colnames(sharedKinaseCorr) <- c("x")
-  #names(sharedKinaseCorr) <- NULL
   
   
   #set where data is from
   bcors$from <- 'random'
   allCorr$from <- 'all'
   sharedKinaseCorr$from <- 'shared'
-  print("after")
   
   allLengths <- rbind(bcors, allCorr)
   allLengths <- rbind(allLengths, sharedKinaseCorr)
   
-  print(allLengths)
-  
-  #normalize y axis
-  
-  ggplot(allLengths, aes(x = x, fill = from)) + geom_density(col=NA, alpha = 0.2)
-  #plot correlation values
-  
-  #plot all pairs/shared-kinase pairs/randomized al pairs
-  
-  #plot historgram
+  ggplot(allLengths, aes(x = x, fill = from)) + geom_density(col=NA, alpha = 0.2) + labs(title="CoPhosphorylation Distribution", x ="CoPhos",  y = "Frequency")
+
 }
 
 realOne = sample(1:20, 8, replace=F) 
@@ -70,7 +62,7 @@ realThree = sample(1:20, 8, replace=F)
 realFour = sample(1:20, 8, replace=F)
 
 data = data.frame(one = realOne, two = realTwo, three = realThree, four = realFour)
-alls = c(0.2, 0.8, 0.9)
+alls = c(-0.2, 0.8, 0.9)
 shared = c(0.9, 0.1, 0.1)
 
 
